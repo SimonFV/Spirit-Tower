a0a71532b8396ed819ae5f64ccf1e096d230184e
SimonFV <sfv0215@gmail.com>
2020-06-20 15:58:04 -0600

    ST-3 ST-2 #done Agregado el cliente en Unity.

---
+++ client/Assets/Scripts/Client.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+public class Client : MonoBehaviour{
+    public static Client client_instance;
+    public static int data_buffer_size = 4096;
+
+    public string server_ip = "127.0.0.1";
+    public int server_port = 54000;
+    public int my_id = 0;
+    public TCP tcp;
+
+    private void Awake(){
+        if(client_instance == null){
+            client_instance = this;
+        }
+        else if(client_instance != this){
+            Destroy(this);
+        }
+    }
+
+    private void Start(){
+        tcp = new TCP();
+    }
+
+    public void conectToServer(){
+        tcp.Conect();
+    }
+
+    public class TCP{
+        TcpClient socket;
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+
+        public void Conect(){
+            socket = new TcpClient{
+                ReceiveBufferSize = data_buffer_size,
+                SendBufferSize = data_buffer_size
+            };
+            receiveBuffer = new byte[data_buffer_size];
+            socket.BeginConnect(client_instance.server_ip, 
+                                client_instance.server_port, 
+                                ConnectCallback, socket);
+        }
+
+        private void ConnectCallback(IAsyncResult _result){
+            socket.EndConnect(_result);
+            if(socket.Connected){
+                return;
+            }
+
+            stream = socket.GetStream();
+            stream.BeginRead(receiveBuffer, 0, data_buffer_size, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult _result){
+            try{
+                int byte_length = stream.EndRead(_result);
+                if(byte_length <= 0){
+                    //TODO: disconnect
+                    return;
+                }
+
+                //TODO: handle data
+                byte[] _data = new byte[byte_length];
+                stream.BeginRead(receiveBuffer, 0, data_buffer_size, ReceiveCallback, null);
+            }
+            catch{
+                //TODO: disconnect
+            }
+        }
+
+    }
+}
---
+++ client/Assets/Scripts/playerMov.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMov : MonoBehaviour{
+    
+    public float movSpeed = 5f;
+    public Rigidbody2D player_rb;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate() {
+        player_rb.MovePosition(player_rb.position + movement*movSpeed*Time.fixedDeltaTime);
+    }
+}
